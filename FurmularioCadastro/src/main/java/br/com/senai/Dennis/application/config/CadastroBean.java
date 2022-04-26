c69c7335f0a233ffe03d9147427675e94542c28a
Aluno <Aluno@DSN1251230723>
2022-04-26 17:27:52 -0300

    Cadsatro

---
+++ FurmularioCadastro/src/main/java/br/com/senai/Dennis/application/config/CadastroBean.java
@@ -0,0 +1,83 @@
+package br.com.senai.Dennis.application.config;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.Flash;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.senai.Dennis.modelo.Linguagem;
+
+@Named("cadastro")
+@RequestScoped
+@SuppressWarnings("serial")
+
+public class CadastroBean implements Serializable {
+
+	private String nome;
+	private String email;
+	private String senha1;
+	private String senha2;
+	private Character sexo;
+	private Boolean receberEmails;
+	private String observacao;
+	private Integer[] linguagens;
+	
+	
+	public Linguagem[] getlistaLinguagens() {
+		
+		return Linguagem.LINGUAGENS;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha1() {
+		return senha1;
+	}
+	public void setSenha1(String senha1) {
+		this.senha1 = senha1;
+	}
+	public String getSenha2() {
+		return senha2;
+	}
+	public void setSenha2(String senha2) {
+		this.senha2 = senha2;
+	}
+	public Character getSexo() {
+		return sexo;
+	}
+	public void setSexo(Character sexo) {
+		this.sexo = sexo;
+	}
+	public Boolean getReceberEmails() {
+		return receberEmails;
+	}
+	public void setReceberEmails(Boolean receberEmails) {
+		this.receberEmails = receberEmails;
+	}
+	public String getObservacao() {
+		return observacao;
+	}
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+	public Integer[] getLinguagens() {
+		return linguagens;
+	}
+	public void setLinguagens(Integer[] linguagens) {
+		this.linguagens = linguagens;
+	}
+	
+	
+}
