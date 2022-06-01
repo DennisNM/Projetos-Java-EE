6c61e831fcbe003af1076145586ab94206c21f10
DennisNM <102614496+DennisNM@users.noreply.github.com>
2022-06-01 08:48:45 -0300

    Update Config.java

--- GeradorNumerico/src/br/com/softblue/javaee/Config.java
+++ GeradorNumerico/src/br/com/softblue/javaee/Config.java
@@ -3,7 +3,7 @@ package br.com.softblue.javaee;
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.annotation.FacesConfig;
 import javax.faces.annotation.FacesConfig.Version;
-
+// config
 @FacesConfig(version = Version.JSF_2_3)
 @ApplicationScoped
 public class Config {
