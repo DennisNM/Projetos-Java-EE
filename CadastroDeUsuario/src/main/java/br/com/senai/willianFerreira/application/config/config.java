1050c95085ca4f2c02d29bd1bdb4d3107c8f2aa5
DennisNM <102614496+DennisNM@users.noreply.github.com>
2022-06-01 08:17:16 -0300

    Update config.java

--- CadastroDeUsuario/src/main/java/br/com/senai/willianFerreira/application/config/config.java
+++ CadastroDeUsuario/src/main/java/br/com/senai/willianFerreira/application/config/config.java
@@ -3,7 +3,7 @@ package br.com.senai.willianFerreira.application.config;
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.annotation.FacesConfig;
 import javax.faces.annotation.FacesConfig.Version;
-
+// config
 @FacesConfig(version = Version.JSF_2_3)
 @ApplicationScoped
 
