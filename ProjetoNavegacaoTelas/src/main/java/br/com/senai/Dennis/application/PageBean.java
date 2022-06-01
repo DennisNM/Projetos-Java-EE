93dc1326aa542e02da56ecca38e8cbaa749693e3
DennisNM <102614496+DennisNM@users.noreply.github.com>
2022-06-01 07:36:25 -0300

    Update PageBean.java

--- ProjetoNavegacaoTelas/src/main/java/br/com/senai/Dennis/application/PageBean.java
+++ ProjetoNavegacaoTelas/src/main/java/br/com/senai/Dennis/application/PageBean.java
@@ -9,22 +9,22 @@ import javax.inject.Named;
 @Named
 @RequestScoped
 public class PageBean implements Serializable {
-
+// string com o nome da pagina
 	private String page;
-	
+	//enum das page
 	public enum pages{
 		page_a,page_b
 	}
-	
+	// Pegar a pagina
 	public String getPage() {
 		return page;
 	}
-
+// setar a apgina
 	public void setPage(String page) {
 		this.page = page;
 	} 	
 
-	
+	// ver que pagian é com if else composto para redirecionar a pagina
 	public pages goTo() {
 		if ("a".equals(page)) {
 			
