66f6d8c97c31c2d54c55667c731c4181397868d6
DennisNM <102614496+DennisNM@users.noreply.github.com>
2022-06-01 09:22:12 -0300

    Update TarefaBean.java

--- JPAEntities/src/main/java/br/com/rafaelcosta/jpa/ejbbean/TarefaBean.java
+++ JPAEntities/src/main/java/br/com/rafaelcosta/jpa/ejbbean/TarefaBean.java
@@ -5,26 +5,26 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import br.com.rafaelcosta.jpa.model.Tarefa;
-
+// tipo sessão EJB
 @Stateless
 public class TarefaBean {
-	//Responsável por gerenciar o banco de dados
+	//Responsável por gerenciar o banco de dados e persistir no banco
 	@PersistenceContext
 	private EntityManager em;
-	
+	// inserir
 	public void inserir(Tarefa tarefa) {
 		em.persist(tarefa);
 	}
-	
+	// atulizar
 	public void atualizar(Tarefa tarefa) {
 		em.merge(tarefa);
 	}
-	
+	// exlcuir do banco
 	public void excluir(Tarefa tarefa) {
 		tarefa = carregar(tarefa.getId());
 		em.remove(tarefa);
 	}
-	
+	// carregar a lista
 	public Tarefa carregar(int id) {
 		return em.find(Tarefa.class, id);
 	}
