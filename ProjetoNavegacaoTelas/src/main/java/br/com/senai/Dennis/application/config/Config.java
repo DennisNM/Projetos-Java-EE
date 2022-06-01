e05589213576c05c8c5397a6abb55dd095aa1323
DennisNM <102614496+DennisNM@users.noreply.github.com>
2022-06-01 07:38:20 -0300

    Update Config.java

--- ProjetoNavegacaoTelas/src/main/java/br/com/senai/Dennis/application/config/Config.java
+++ ProjetoNavegacaoTelas/src/main/java/br/com/senai/Dennis/application/config/Config.java
@@ -3,7 +3,7 @@ package br.com.senai.Dennis.application.config;
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.annotation.FacesConfig;
 import javax.faces.annotation.FacesConfig.Version;
-
+// configurarar jsf
 @FacesConfig(version = Version.JSF_2_3) 
 @ApplicationScoped
 public class Config {
